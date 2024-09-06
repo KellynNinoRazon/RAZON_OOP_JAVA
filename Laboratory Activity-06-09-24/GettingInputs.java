456a84566a4ba0e1f514ff1feaf1630dc74f2e92
KellynNinoRazon <163124264+KellynNinoRazon@users.noreply.github.com>
2024-09-06 10:50:11 +0000

    Act 1.1 Done!

---
+++ Laboratory Activity-06-09-24/GettingInputs.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GettingInputs
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        int year = scan.nextInt();
+        scan.nextLine();
+        
+        System.out.print("Enter the genre: ");
+        String genre = scan.nextLine();
+        
+        System.out.print("Enter the album: ");
+        String album = scan.nextLine();
+
+        System.out.print("Enter the song: ");
+        String song = scan.nextLine();
+
+        System.out.print("Enter the artist: ");
+        String artist = scan.nextLine();
+
+        System.out.println("---------------------------------------------");
+        System.out.println("SONG DETAILS");
+        System.out.println("---------------------------------------------");
+
+        System.out.println("Year Released: "+ year + "\nGenre: "+ genre + "\nAlbum: "+ album + "\nTitle: \"" + song + "\"" + "\nArtist: " + artist );
+        scan.close();
+    }
+}
