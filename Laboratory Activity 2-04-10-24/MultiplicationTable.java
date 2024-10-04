8214557aefc5194ba43b2c32d2174cba53d67550
KellynNinoRazon <163124264+KellynNinoRazon@users.noreply.github.com>
2024-10-04 15:15:45 +0000

    Finallyy!

--- Laboratory Activity 2-04-10-24/MultiplicationTable.java
+++ Laboratory Activity 2-04-10-24/MultiplicationTable.java
@@ -4,8 +4,28 @@ public class MultiplicationTable
 {
     public static void main(String[] args)
     {
-        Scanner scan= new Scanner(System.in)
+        Scanner scan= new Scanner(System.in);
         System.out.print("Enter the size of the Multiplication Table: ");
+        int max= scan.nextInt();
+
+        int [][]multiples= new int[max][max];
+
+        for (int ctrRow=0;ctrRow<max;ctrRow++)
+        {
+            for (int ctrCol=0;ctrCol<max;ctrCol++)
+            {
+                multiples[ctrRow][ctrCol]= (ctrRow + 1) * (ctrCol + 1);
+            }
+        }
+        System.out.println("Multiplication Table: ");
+
+        for (int ctrRow=0;ctrRow<max;ctrRow++)
+        {
+            for (int ctrCol=0;ctrCol<max;ctrCol++)
+            System.out.printf("%4d",multiples[ctrRow][ctrCol]);
+            System.out.println();
+        }
+        scan.close();
     }
     
     
