990fffe50fcb09b193f91e79aefd9641716d628f
KellynNinoRazon <163124264+KellynNinoRazon@users.noreply.github.com>
2024-10-04 11:10:58 +0000

    kells

---
+++ Laboratory Activity 2-04-10-24/MultiplicationTable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MultiplicationTable 
+{
+    public static void main(String[] args)
+    {
+        Scanner scan= new Scanner(System.in)
+        System.out.print("Enter the size of the Multiplication Table: ");
+    }
+    
+    
+}
